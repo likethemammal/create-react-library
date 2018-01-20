69de96fa39ee656eed01621209b1083ba614b86b
Chris Dolphin <cdolphin1991@gmail.com>
2018-01-19 21:12:07 -0800

    Add support for react-primitives

--- src/lib/components/Example.js
+++ src/lib/components/Example.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import udiliaLogo from './udilia-logo.svg';
-import './Example.css';
+
+import {
+    View,
+    StyleSheet,
+    Text,
+} from '@likethemammal/react-primitives'
+
+
+const styles = StyleSheet.create({
+    Example: {
+        marginTop: 60,
+        alignItems: 'center',
+    },
+    Example_text: {
+        color: '#333',
+        fontSize: 32,
+        fontWeight: '300',
+        textAlign: 'center',
+        paddingTop: 20,
+    }
+})
 
 const Example = () => (
-  <div className="Example">
+  <View style={styles.Example}>
     <img src={udiliaLogo} alt="React logo" width="62" />
-    <h1 className="Example-text">Create React Library</h1>
-  </div>
+    <Text style={styles.Example_text}>Create React-Primitives Library</Text>
+  </View>
 );
 
 export default Example;
